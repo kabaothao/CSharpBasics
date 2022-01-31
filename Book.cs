341ceba36bf08797799d01de00d2004bf0f41e44
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-01-31 10:26:25 -0600

    add class

---
+++ Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		//When we create an object, we're creating an actualy physical book inside of our program.
+		//Generally when you name class you want to name it with a capital letter.
+		//so create a book data type. You want to create a book with their attributes.
+		//class allow us to specify a custom data type.
+
+
+	public string title;
+	public string author;
+	public int pages;
+
+	}
+}
